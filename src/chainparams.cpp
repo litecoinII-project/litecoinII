a882a0b49753561ff391bbc2e02cf60049c8bd01
litecoinII-dev <266516605+litecoinII-dev@users.noreply.github.com>
2026-03-09 23:03:03 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -239,7 +239,8 @@ public:
         bech32_hrp = "tlc2";
         mweb_hrp = "tmweb";
 
-        vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_test), std::end(chainparams_seed_test));
+        vFixedSeeds.clear();
+        //vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_test), std::end(chainparams_seed_test));
 
         fDefaultConsistencyChecks = false;
         fRequireStandard = false;
